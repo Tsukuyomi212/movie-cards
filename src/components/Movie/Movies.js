c604816a06323db88e6150f996638ffba52ad25f
Tsukuyomi212 <nada.zelic@gmail.com>
2019-12-23 13:24:31 +0100

    2 - delete movies

--- src/components/Movie/Movies.js
+++ src/components/Movie/Movies.js
@@ -22,13 +22,19 @@ export default class Movies extends Component {
         this.setState({movies: [...movies, movie]});
     }
 
+    removeMovie(movieId) {
+        const {movies} = this.state;
+        const updatedMovies = movies.filter(movie => movie.id !== movieId);
+        this.setState({movies: updatedMovies});
+    }
+
     render() {
 
         return (
             <div className="container-fluid" style={{marginLeft: '-15px'}}>
                 <div className="d-flex flex-row">                    
                     <div className="col-sm-12">
-                        <MovieList movies={this.state.movies} />
+                        <MovieList movies={this.state.movies} removeMovie={this.removeMovie}/>
                         <NewMovieForm addMovie={(movie) => this.addMovie(movie)}/>
                     </div>
                 </div>
